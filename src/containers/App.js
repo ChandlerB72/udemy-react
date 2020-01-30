ac2aef7e2a620634c39387bdc7148e7e817526f4
Chandler Brown <chandler_brown@homedepot.com>
2020-01-30 11:25:11 -0500

    Added title prop to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -67,6 +67,7 @@ class App extends Component {
     return (
         <div className={classes.App}>
           <Cockpit 
+            title={this.props.title}
             showPersons={this.state.showPersons}
             persons={this.state.persons}
             clicked={this.togglePersonHandler}/>
